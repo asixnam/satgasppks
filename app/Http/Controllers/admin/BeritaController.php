da74fabc7dd9370afa002f5671c1bd1df01702ff
Asix1234 <asixartmedia@gmail.com>
2025-07-24 04:54:40 +0700

    berita

--- app/Http/Controllers/admin/BeritaController.php
+++ app/Http/Controllers/admin/BeritaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Berita;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage; 
 
 class BeritaController extends Controller
 {
